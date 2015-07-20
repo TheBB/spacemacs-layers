4bb6524bb3d19ea0d7ebc479764ab21dfe5d6abb
Eivind Fonn <evfonn@gmail.com>
2015-07-20 14:37:50 +0200

    Add some missing headers

--- modify-theme/config.el
+++ modify-theme/config.el
@@ -19,13 +19,25 @@ or the symbol `all'.")
 (defvar modify-theme--modified-faces '())
 
 (defvar modify-theme--header-faces
-  '(font-latex-slide-title-face
-    font-latex-sectioning-0-face
+  '(font-latex-sectioning-0-face
     font-latex-sectioning-1-face
     font-latex-sectioning-2-face
     font-latex-sectioning-3-face
     font-latex-sectioning-4-face
     font-latex-sectioning-5-face
+    font-latex-slide-title-face
+    info-title-1
+    info-title-2
+    info-title-3
+    info-title-4
+    markdown-header-face
+    markdown-header-face-1
+    markdown-header-face-2
+    markdown-header-face-3
+    markdown-header-face-4
+    markdown-header-face-5
+    markdown-header-face-6
+    org-document-title
     org-level-1
     org-level-2
     org-level-3
@@ -33,14 +45,7 @@ or the symbol `all'.")
     org-level-5
     org-level-6
     org-level-7
-    org-level-8
-    markdown-header-face
-    markdown-header-face-1
-    markdown-header-face-2
-    markdown-header-face-3
-    markdown-header-face-4
-    markdown-header-face-5
-    markdown-header-face-6))
+    org-level-8))
 
 (advice-add 'load-theme :after 'modify-theme/load-theme)
 (modify-theme/load-theme spacemacs--cur-theme)
