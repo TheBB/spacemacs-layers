9613ecf02826f15090d388b6b16ee4e7b5d4b6ce
Eivind Fonn <evfonn@gmail.com>
2015-07-20 14:26:51 +0200

    Add modify-theme layer

---
+++ modify-theme/config.el
@@ -0,0 +1,46 @@
+(defvar modify-theme-modifications '()
+  "An alist of theme modifications. Each element should
+be on the form (THEME . SPEC), where THEME is a symbol
+representing a theme, and SPEC is an alist mapping faces
+to face specs (see `defface').")
+
+(defvar modify-theme-headings-inherit-from-default '()
+  "A list of themes where all headings should inherit
+from the default face, or the symbol `all'.")
+
+(defvar modify-theme-headings-same-size '()
+  "A list of themes where all headings should have the
+same size, or the symbol `all'.")
+
+(defvar modify-theme-headings-bold '()
+  "A list of themes where all headings should be bold,
+or the symbol `all'.")
+
+(defvar modify-theme--modified-faces '())
+
+(defvar modify-theme--header-faces
+  '(font-latex-slide-title-face
+    font-latex-sectioning-0-face
+    font-latex-sectioning-1-face
+    font-latex-sectioning-2-face
+    font-latex-sectioning-3-face
+    font-latex-sectioning-4-face
+    font-latex-sectioning-5-face
+    org-level-1
+    org-level-2
+    org-level-3
+    org-level-4
+    org-level-5
+    org-level-6
+    org-level-7
+    org-level-8
+    markdown-header-face
+    markdown-header-face-1
+    markdown-header-face-2
+    markdown-header-face-3
+    markdown-header-face-4
+    markdown-header-face-5
+    markdown-header-face-6))
+
+(advice-add 'load-theme :after 'modify-theme/load-theme)
+(modify-theme/load-theme spacemacs--cur-theme)
---
+++ modify-theme/funcs.el
@@ -0,0 +1,30 @@
+(defun modify-theme//in-or-all (key seq)
+  (or (eq 'all seq) (memq key seq)))
+
+(defun modify-theme/load-theme (theme &optional no-confirm no-enable)
+  (unless no-enable
+
+    ;; Remove existing modifications
+    (dolist (face modify-theme--modified-faces)
+      (custom-set-faces `(,face ((t ())))))
+    (setq modify-theme--modified-faces nil)
+
+    ;; Headings
+    (let ((mods nil))
+      (when (modify-theme//in-or-all theme modify-theme-headings-inherit-from-default)
+        (setq mods (plist-put mods :inherit 'default)))
+      (when (modify-theme//in-or-all theme modify-theme-headings-same-size)
+        (setq mods (plist-put mods :height 1.0)))
+      (when (modify-theme//in-or-all theme modify-theme-headings-bold)
+        (setq mods (plist-put mods :weight 'bold)))
+      (when mods
+        (dolist (face modify-theme--header-faces)
+          (custom-set-faces `(,face ((t ,mods))))
+          (push face modify-theme--modified-faces))))
+
+    ;; Add new modifications
+    (let ((mods (append (cdr (assq theme modify-theme-modifications))
+                        (cdr (assq t modify-theme-modifications)))))
+      (dolist (spec mods)
+        (custom-set-faces spec)
+        (push (car spec) modify-theme--modified-faces)))))
