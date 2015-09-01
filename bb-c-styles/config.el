428b2fa29f9f62749d4f3491e7561fa7b7f22d79
Eivind Fonn <evfonn@gmail.com>
2015-09-01 13:36:57 +0200

    Add bb-c-styles

---
+++ bb-c-styles/config.el
@@ -0,0 +1,35 @@
+(c-add-style "bb"
+             '((indent-tabs-mode . nil)
+               (c-basic-offset . 4)
+               (c-offsets-alist
+                (substatement-open . 0)
+                (inline-open . 0)
+                (statement-cont . c-lineup-assignments)
+                (inextern-lang . 0)
+                (innamespace . 0))))
+
+(c-add-style "sintef"
+             '((indent-tabs-mode . nil)
+               (c-basic-offset . 2)
+               (c-offsets-alist
+                (substatement-open . 0)
+                (inline-open . 0)
+                (statement-cont . c-lineup-assignments)
+                (inextern-lang . 0)
+                (innamespace . 0))))
+
+(setq-default c-macro-names-with-semicolon
+              '("Q_OBJECT"
+                "Q_PROPERTY"
+                "Q_DECLARE"
+                "Q_ENUMS"
+                "Q_INTERFACES"))
+
+(add-hook 'c-mode-common-hook
+          (defun bb-c-styles/set-c-style ()
+            (c-set-style "bb")
+            (c-make-macro-with-semi-re)))
+
+(dolist (mode '(c-mode c++-mode))
+  (evil-leader/set-key-for-mode mode
+    "mos" 'c-set-style))
