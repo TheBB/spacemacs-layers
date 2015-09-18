0ea6ba428ce570c40e6d4ffac5885be006feaa19
Eivind Fonn <evfonn@gmail.com>
2015-09-18 12:16:05 +0200

    Add a bunch of modes to evil-shift-width

--- evil-shift-width/config.el
+++ evil-shift-width/config.el
@@ -1,6 +1,34 @@
+(defun evil-shift-width/latex-mode ()
+  (or (and (boundp 'LaTeX-indent-level) LaTeX-indent-level)
+      (and (boundp 'tex-indent-basic) tex-indent-basic)))
+
+;; Thanks to editorconfig-emacs for many of these
 (defvar evil-shift-width-alist
-  '((python-mode . python-indent-offset)
-    ((c-mode c++-mode java-mode) . c-basic-offset))
+  '(((awk-mode c-mode c++-mode java-mode groovy-mode
+      idl-mode java-mode objc-mode pike-mode) . c-basic-offset)
+    (python-mode . python-indent-offset)
+    (cmake-mode . cmake-tab-width)
+    (coffee-mode . coffee-tab-width)
+    (cperl-mode . cperl-indent-level)
+    (css-mode . css-indent-offset)
+    ((emacs-lisp-mode lisp-mode) . lisp-indent-offset)
+    (enh-ruby-mode . enh-ruby-indent-level)
+    (erlang-mode . erlang-indent-level)
+    ((js-mode json-mode) . js-indent-level)
+    (js2-mode . js2-basic-offset)
+    (js3-mode . js3-indent-level)
+    (latex-mode . (evil-shift-width/latex))
+    (livescript-mode . livescript-tab-width)
+    (mustache-mode . mustache-basic-offset)
+    (nxml-mode . nxml-child-indent)
+    (perl-mode . perl-indent-level)
+    (puppet-mode . puppet-indent-level)
+    (ruby-mode . ruby-indent-level)
+    (scala-mode . scala-indent:step)
+    (sgml-mode . sgml-basic-offset)
+    (sh-mode . sh-basic-offset)
+    (web-mode . web-mode-markup-indent-offset)
+    (yaml-mode . yaml-indent-offset))
   "An alist where each key is either a symbol corresponding
 to a major mode, a list of such symbols, or the symbol t,
 acting as default. The values are either integers or forms
@@ -17,8 +45,8 @@ which are evaluated upon entering a major mode.")
                 (when (or (and (symbolp mode) (derived-mode-p mode))
                           (and (listp mode) (apply 'derived-mode-p mode))
                           (eq 't mode))
-                  (throw 'break
-                         (if (integerp val) val (eval val))))))
+                  (throw 'break (cond ((integerp val) val)
+                                      (t (eval val)))))))
             (throw 'break evil-shift-width))))
      (if (and (integerp shift-width)
               (< 0 shift-width))
