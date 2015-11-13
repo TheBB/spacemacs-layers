f13942ffac5d57283a359c7a5a2fb7fbd61ad6ea
Eivind Fonn <evfonn@gmail.com>
2015-11-13 17:53:24 +0100

    Fix no-dots up-level when empty folder

--- no-dots/funcs.el
+++ no-dots/funcs.el
@@ -1,3 +1,15 @@
+(require 'cl-lib)
+
 (defun no-dots/helm-ff-filter-candidate-one-by-one (fcn file)
   (unless (string-match "\\(?:/\\|\\`\\)\\.\\{1,2\\}\\'" file)
     (funcall fcn file)))
+
+(defun no-dots/helm-file-completion-source-p (&rest args) t)
+
+(defun no-dots/helm-find-files-up-one-level (fcn &rest args)
+  (prog2
+      (advice-add 'helm-file-completion-source-p
+                  :around 'no-dots/helm-file-completion-source-p)
+      (apply fcn args)
+    (advice-remove 'helm-file-completion-source-p
+                   'no-dots/helm-file-completion-source-p)))
--- no-dots/packages.el
+++ no-dots/packages.el
@@ -4,4 +4,6 @@
 (defun no-dots/post-init-helm ()
   (with-eval-after-load 'helm-files
     (advice-add 'helm-ff-filter-candidate-one-by-one
-                :around 'no-dots/helm-ff-filter-candidate-one-by-one)))
+                :around 'no-dots/helm-ff-filter-candidate-one-by-one)
+    (advice-add 'helm-find-files-up-one-level
+                :around 'no-dots/helm-find-files-up-one-level)))
