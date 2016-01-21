106d7c33415e362d1aafa8d4dc9e7166f0fafb97
Eivind Fonn <evfonn@gmail.com>
2016-01-21 12:25:35 +0100

    Oops

--- init.el
+++ init.el
@@ -94,7 +94,7 @@
    dotspacemacs-elpa-https t
    dotspacemacs-elpa-timeout 10
    dotspacemacs-check-for-update t
-   dotspacemacs-editing-style 'hybrid
+   dotspacemacs-editing-style 'vim
    dotspacemacs-startup-banner nil
    dotspacemacs-startup-lists '(recents bookmarks projects)
    dotspacemacs-startup-recent-list-size 5
