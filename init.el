c2c51e80e33159ab7617b64ffa640cfa147b3294
Eivind Fonn <evfonn@gmail.com>
2016-02-03 17:09:58 +0100

    Associate .C with .h

--- init.el
+++ init.el
@@ -348,6 +348,7 @@
                ("h" . c++-mode)))
     (push (cons (concat "\\." (car e) "\\'") (cdr e)) auto-mode-alist))
   (push '("PKGBUILD" . shell-script-mode) auto-mode-alist)
+  (push '("C" "h") projectile-other-file-alist)
 
   ;; Keybindings
   (bb/define-key evil-normal-state-map
