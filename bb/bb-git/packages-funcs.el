841be3c9075a57e2485f391e73ac22c21a479f4d
Eivind Fonn <evfonn@gmail.com>
2016-02-23 17:26:50 +0100

    Forgot that this function is GPL'd

--- bb/bb-git/packages-funcs.el
+++ bb/bb-git/packages-funcs.el
@@ -1,3 +1,18 @@
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; For a full copy of the GNU General Public License
+;; see <http://www.gnu.org/licenses/>.
+
+;; Thanks Jonas Bernoulli
+
 (defun magit-branch-pull-request (number &optional branch checkout)
   "Create a new branch from a Github pull request and show its log.
 Read \"NR[:BRANCH-NAME] from the user.  If BRANCH-NAME is not
