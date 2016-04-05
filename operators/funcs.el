3e00334f21b116b48c1cec726d82eb1e721bfd16
Eivind Fonn <evfonn@gmail.com>
2016-04-05 11:16:01 +0200

    Fix operators

--- operators/funcs.el
+++ operators/funcs.el
@@ -1,8 +1,7 @@
-(evil-define-operator operators-narrow (beg end)
-  (narrow-to-region beg end))
-
-(evil-define-operator operators-gist-public (beg end)
-  (gist-region beg end))
-
-(evil-define-operator operators-gist-private (beg end)
-  (gist-region beg end 'private))
+(with-eval-after-load 'evil
+  (evil-define-operator operators-narrow (beg end)
+    (narrow-to-region beg end))
+  (evil-define-operator operators-gist-public (beg end)
+    (gist-region beg end))
+  (evil-define-operator operators-gist-private (beg end)
+    (gist-region beg end 'private)))
