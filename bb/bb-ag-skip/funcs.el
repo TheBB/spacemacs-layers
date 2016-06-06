51c956e6b1bc8a93922889084911a16d4e6ea3ce
Eivind Fonn <evfonn@gmail.com>
2016-06-06 23:58:43 +0200

    Use next-error-function instead

--- bb/bb-ag-skip/funcs.el
+++ bb/bb-ag-skip/funcs.el
@@ -1,27 +1,20 @@
 (defvar-local bb--ag-tracker nil)
+(defvar-local bb--ag-max nil)
 
-(defun bb/helm-ag--action-find-file-before (candidate)
-  (with-current-buffer "*helm-ag*"
-    (setq bb--ag-tracker (point))))
+(defun bb/ag-skip-init (&rest args)
+  (with-current-buffer "*helm ag results*"
+    (setq bb--ag-tracker 5)
+    (setq bb--ag-max (save-excursion
+                       (goto-char (point-max))
+                       (previous-line)
+                       (line-number-at-pos)))
+    (setq next-error-function 'bb/ag-skip-next)))
 
-(defmacro bb|ag-skipper (name func)
-  `(defun ,name ()
-     (interactive)
-     (let ((candidate
-            (with-current-buffer "*helm-ag*"
-              (when bb--ag-tracker (goto-char bb--ag-tracker))
-              (,func)
-              (let ((str (buffer-substring (point-at-bol) (point-at-eol))))
-                (when (and str (< 0 (length str))
-                           (< 1 (line-number-at-pos)))
-                  (setq bb--ag-tracker (point))
-                  (message
-                   "Match %d/%d"
-                   (- (line-number-at-pos) 1)
-                   (- (save-excursion (goto-char (point-max)) (line-number-at-pos)) 2))
-                  str)))))
-       (when candidate
-         (helm-ag--find-file-action candidate 'find-file nil)))))
-
-(bb|ag-skipper bb/ag-next next-line)
-(bb|ag-skipper bb/ag-prev previous-line)
+(defun bb/ag-skip-next (num reset)
+  (let ((cand (with-current-buffer "*helm ag results*"
+                (save-excursion
+                  (when reset (setq bb--ag-tracker 5))
+                  (setq bb--ag-tracker (min bb--ag-max (max 5 (+ bb--ag-tracker num))))
+                  (goto-line bb--ag-tracker)
+                  (helm-current-line-contents)))))
+    (helm-ag--find-file-action cand 'find-file helm-ag--search-this-file-p)))
--- bb/bb-ag-skip/packages.el
+++ bb/bb-ag-skip/packages.el
@@ -1,8 +1,6 @@
 (defconst bb-ag-skip-packages '(helm-ag))
 
 (defun bb-ag-skip/post-init-helm-ag ()
-  (define-key evil-normal-state-map (kbd "] a") 'bb/ag-next)
-  (define-key evil-normal-state-map (kbd "[ a") 'bb/ag-prev)
   (with-eval-after-load 'helm-ag
-    (advice-add 'helm-ag--action-find-file
-                :before 'bb/helm-ag--action-find-file-before)))
+    (advice-add 'helm-ag--save-results
+                :after 'bb/ag-skip-init)))
