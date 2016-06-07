e1a156e2800dfa4cb6fc2f3bd227a625b2915213
Eivind Fonn <evfonn@gmail.com>
2016-06-07 14:15:06 +0200

    Ivy support

--- bb/bb-ag-skip/packages.el
+++ bb/bb-ag-skip/packages.el
@@ -1,5 +1,10 @@
-(defconst bb-ag-skip-packages '(helm
+(defconst bb-ag-skip-packages '(ivy
+                                helm
                                 helm-ag))
+
+(defun bb-ag-skip/post-init-ivy ()
+  (with-eval-after-load 'ivy
+    (define-key ivy-minibuffer-map (kbd "<f3>") 'bb/ivy-save-to-buffer)))
 
 (defun bb-ag-skip/post-init-helm ()
   (with-eval-after-load 'helm-grep
