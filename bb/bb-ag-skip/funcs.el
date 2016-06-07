985061b00c1fb6b955635826494f86569a4935a9
Eivind Fonn <evfonn@gmail.com>
2016-06-07 12:14:27 +0200

    Grep support

--- bb/bb-ag-skip/funcs.el
+++ bb/bb-ag-skip/funcs.el
@@ -1,8 +1,10 @@
 (defvar-local bb--ag-tracker nil)
 (defvar-local bb--ag-max nil)
+(defvar bb--ag-buf nil)
 
 (defun bb/ag-skip-init (&rest args)
   (with-current-buffer "*helm ag results*"
+    (setq bb--ag-buf (current-buffer))
     (setq bb--ag-tracker 5)
     (setq bb--ag-max (save-excursion
                        (goto-char (point-max))
@@ -11,10 +13,31 @@
     (setq next-error-function 'bb/ag-skip-next)))
 
 (defun bb/ag-skip-next (num reset)
-  (let ((cand (with-current-buffer "*helm ag results*"
+  (let ((cand (with-current-buffer bb--ag-buf
                 (save-excursion
                   (when reset (setq bb--ag-tracker 5))
                   (setq bb--ag-tracker (min bb--ag-max (max 5 (+ bb--ag-tracker num))))
                   (goto-line bb--ag-tracker)
-                  (helm-current-line-contents)))))
+                  (buffer-substring (point-at-bol) (point-at-eol))))))
     (helm-ag--find-file-action cand 'find-file helm-ag--search-this-file-p)))
+
+(defun bb/grep-skip-init (&rest args)
+  (with-current-buffer "*hgrep*"
+    (setq bb--ag-buf (current-buffer))
+    (setq bb--ag-tracker 5)
+    (setq bb--ag-max (save-excursion
+                       (goto-char (point-max))
+                       (previous-line)
+                       (line-number-at-pos)))
+    (setq next-error-function 'bb/grep-skip-next)))
+
+(defun bb/grep-skip-next (num reset)
+  (let ((cand (with-current-buffer bb--ag-buf
+                (save-excursion
+                  (when reset (setq bb--ag-tracker 5))
+                  (setq bb--ag-tracker (min bb--ag-max (max 5 (+ bb--ag-tracker num))))
+                  (goto-line bb--ag-tracker)
+                  (buffer-substring (point-at-bol) (point-at-eol))))))
+    (with-current-buffer bb--ag-buf
+      (goto-line bb--ag-tracker)
+      (helm-grep-action cand))))
--- bb/bb-ag-skip/packages.el
+++ bb/bb-ag-skip/packages.el
@@ -1,6 +1,10 @@
-(defconst bb-ag-skip-packages '(helm-ag))
+(defconst bb-ag-skip-packages '(helm
+                                helm-ag))
+
+(defun bb-ag-skip/post-init-helm ()
+  (with-eval-after-load 'helm-grep
+    (advice-add 'helm-grep-save-results-1 :after 'bb/grep-skip-init)))
 
 (defun bb-ag-skip/post-init-helm-ag ()
   (with-eval-after-load 'helm-ag
-    (advice-add 'helm-ag--save-results
-                :after 'bb/ag-skip-init)))
+    (advice-add 'helm-ag--save-results :after 'bb/ag-skip-init)))
