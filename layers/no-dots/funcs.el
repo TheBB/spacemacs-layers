d32d5aaa0762906f518bbb3bca75c710b8fb2248
Eivind Fonn <evfonn@gmail.com>
2017-01-26 11:16:54 +0100

    Update no-dots

--- layers/no-dots/funcs.el
+++ layers/no-dots/funcs.el
@@ -10,10 +10,19 @@
 
 (defun no-dots/helm-file-completion-source-p (&rest args) t)
 
+(defun no-dots/helm-attrset (fcn attribute-name value &optional src)
+  (let ((src (or src (helm-get-current-source))))
+    (when src
+      (funcall fcn attribute-name value src))))
+
 (defun no-dots/helm-find-files-up-one-level (fcn &rest args)
-  (prog2
-      (advice-add 'helm-file-completion-source-p
-                  :around 'no-dots/helm-file-completion-source-p)
-      (apply fcn args)
+  (advice-add 'helm-file-completion-source-p
+              :around 'no-dots/helm-file-completion-source-p)
+  (advice-add 'helm-attrset
+              :around 'no-dots/helm-attrset)
+  (let ((res (apply fcn args)))
     (advice-remove 'helm-file-completion-source-p
-                   'no-dots/helm-file-completion-source-p)))
+                   'no-dots/helm-file-completion-source-p)
+    (advice-remove 'helm-attrset
+                   'no-dots/helm-attrset)
+    res))
